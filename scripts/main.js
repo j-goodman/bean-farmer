c7b333331f76281d33bb5adeed2a7ec46488c3af
j-goodman <johncurtisgoodman@gmail.com>
2024-01-26 12:34:56 -0500

    Seperate image loading into its own file.

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,6 @@
 import { game } from './game.js';
 import { temporaryWorldSetup } from './temporaryWorldSetup.js'
+import { imageLoader } from './imageLoader.js'
 
 temporaryWorldSetup()
 
@@ -23,75 +24,7 @@ const addImage = (name) => {
     }
 }
 
-addImage("blob-down")
-addImage("blob-down-left-1")
-addImage("blob-down-left-2")
-addImage("blob-left")
-addImage("blob-left-up-1")
-addImage("blob-left-up-2")
-addImage("blob-up")
-addImage("blob-right-up-2")
-addImage("blob-right-up-1")
-addImage("blob-right")
-addImage("blob-down-right-2")
-addImage("blob-down-right-1")
-addImage("blob-hurt-1")
-addImage("blob-hurt-2")
-addImage("select-box")
-addImage("rock")
-addImage("boulder")
-addImage("ore")
-addImage("wooly-pig-up")
-addImage("wooly-pig-left-up-0")
-addImage("wooly-pig-left-up-1")
-addImage("wooly-pig-left-up-2")
-addImage("wooly-pig-left-up-3")
-addImage("wooly-pig-left-up-4")
-addImage("wooly-pig-right")
-addImage("wooly-pig-right-up-0")
-addImage("wooly-pig-right-up-1")
-addImage("wooly-pig-right-up-2")
-addImage("wooly-pig-right-up-3")
-addImage("wooly-pig-right-up-4")
-addImage("wooly-pig-down-right-1")
-addImage("wooly-pig-down-right-2")
-addImage("wooly-pig-down-right-3")
-addImage("wooly-pig-down")
-addImage("wooly-pig-left")
-addImage("wooly-pig-down-left-1")
-addImage("wooly-pig-down-left-2")
-addImage("wooly-pig-down-left-3")
-addImage("heart")
-addImage("blob-red-flash")
-addImage("corn-pink")
-
-for (let i = 1; i <= 10; i++) {
-    addImage(`wooly-pig-attack-right/${i}`)
-}
-
-for (let i = 1; i <= 10; i++) {
-    addImage(`wooly-pig-attack-left/${i}`)
-}
-
-for (let i = 1; i <= 10; i++) {
-    addImage(`wooly-pig-attack-up/${i}`)
-}
-
-for (let i = 1; i <= 11; i++) {
-    addImage(`wooly-pig-attack-down/${i}`)
-}
-
-for (let i = 1; i <= 12; i++) {
-    addImage(`rock-break/${i}`)
-}
-
-for (let i = 0; i <= 10; i++) {
-    addImage(`heart-burst/${i}`)
-}
-
-for (let i = 1; i <= 14; i++) {
-    addImage(`blob-bubbles/${i}`)
-}
+imageLoader(addImage)
 
 const gameLoop = () => {
     game.ctx.clearRect(0, 0, game.canvas.width, game.canvas.height);
