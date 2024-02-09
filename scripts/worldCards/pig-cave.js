42f24e78731a183443986bcdc248a7a8c0fd463f
j-goodman <johncurtisgoodman@gmail.com>
2024-02-09 16:08:53 -0500

    Fix spawn point.

--- scripts/worldCards/pig-cave.js
+++ scripts/worldCards/pig-cave.js
@@ -26,7 +26,7 @@ let pigCave = new WorldCard (
     `       XX XX   XXXXXX XO        `,
     `        XXX     XXX XXXXX       `,
     `         XX  P      XXXXX X     `,
-    `       X sX s  DXXX XXXX        `,
+    `       X sX     XXX XXXX        `,
     `       s XXX   XX X  XX         `,
     `        sXXXXXXXXXXX XXX       O`,
     `      X XXX XXX  XXX XX         `,
