f4543f45abeb562ea4fccefda87f4556d9ebcd51
j-goodman <johncurtisgoodman@gmail.com>
2025-10-03 12:40:23 -0400

    Add magic rings.

---
+++ scripts/rings.js
@@ -0,0 +1,73 @@
+import { Item } from './item.js';
+import { Sprite } from './sprite.js';
+
+class Ring extends Item {
+    constructor(x, y) {
+        super(x, y)
+        this.name = "pearl ring"
+        this.color = "pearl"
+        this.pearlSprite = new Sprite ("pearl-ring")
+        this.onyxSprite = new Sprite ("onyx-ring")
+        this.sprite = this.pearlSprite
+        game.setTimer(() => {
+            this.findPartner()
+        }, 30)
+    }
+    
+    setVariant (color) {
+        if (color === "onyx") {
+            this.variant = "onyx"
+            this.color = "onyx"
+            this.name = "onyx ring"
+            this.sprite  = new Sprite ("onyx-ring")
+            this.use = null
+        }
+    }
+
+    findPartner () {
+        for (let x = -88; x < 128; x++) {
+            for (let y = -60; y < 128; y++) {
+                const item = game.checkGrid(x, y)
+                if (item && item.name !== this.name && (item.name === "pearl ring" || item.name === "onyx ring")) {
+                    this.partner = item
+                }
+            }
+        }
+    }
+
+    use (target) {
+        if (this.partner && this.partner.exists) {
+            const oldColor = this.color
+            const newColor = this.color === "pearl" ? "onyx" : "pearl"
+            this.name = `${newColor} ring`
+            this.sprite = this[`${newColor}Sprite`]
+            this.partner.name = `${oldColor} ring`
+            this.partner.sprite = this.partner[`${oldColor}Sprite`]
+            this.color = newColor
+            this.partner.color = oldColor
+            const targetX = target.position.x
+            const targetY = target.position.y
+            target.teleport(this.partner.position)
+            this.partner.position = {x: targetX, y: targetY}
+            game.checkGrid(targetX, targetY, true).occupant = this.partner
+            this.partner.spritePosition = {x: targetX, y: targetY}
+        }
+    }
+
+    // teleport (target) {
+        // if (this.partner && this.partner.exists) {
+        //     target.dropItem()
+        //     target.pickUpItem(this.partner)
+        //     this.partner.die()
+        //     this.die()
+        //     target.die()
+        //     game.addToGrid(this, (target.position.x, target.position.y))
+        //     target.position = this.partner.position
+        //     game.addToGrid(target, (this.partner.position.x, this.partner.position.y))
+        //     target.spritePosition = this.partner.position
+        // }
+    // }
+}
+
+game.constructors[Ring.name] = Ring
+export { Ring }
